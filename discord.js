131928fc30a261ac32c3f45ba21464e652ed29e8
vreezy <vreezy117@googlemail.com>
2022-08-14 19:08:44 +0200

    zwischenspeichern

--- discord.js
+++ discord.js
@@ -1,8 +1,16 @@
 (function( discord, $, undefined ) {
 
 const iframeContainerClass = `egj-discord-iframe-container`;
-const iFrameContainerId = `discordIframeContainer`;
+const iFrameContainerId = `egjDiscordIframeContainer`;
 
+const bubbleContainerId =  `egjDiscordBubbleContainer`; 
+
+const privacyContainerId = `egjDiscordPrivacyContainer`;
+
+const privacyButtonId = `egjDiscordPrivacyButton`; 
+
+discord.privacyAccepted = false;
+discord.privacyContainerCreated = false;
 
 function getVW() {
    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);;
@@ -13,6 +21,7 @@ function getVH() {
 }
 
 function getData() {
+   console.log("egj discord getData");
    $.get( '/wp-json/erfindergeist/v1/discord')
       .done(function( data ) {
          render(data);   
@@ -25,6 +34,7 @@ function getData() {
 }
 
 function renderError() {
+   console.log("egj discord renderError.");
    var htmlIframeString = `
       <div id="${iFrameContainerId}" class="${iframeContainerClass} egj-discord-fade-in">
          Error Loading data.
@@ -33,12 +43,16 @@ function renderError() {
 
    createDiv(htmlIframeString);
 
-   container = document.getElementById(iFrameContainerId);
+   const bubbleContainer = $( `#${bubbleContainerId}` ).first();
+   const iFrameContainer = $( `#${iFrameContainerId}` ).first();
 
-   setDisplay(container, "block");
+   const privacyCSSTop = bubbleContainer.position().top - iFrameContainer.height() - 10;
+   iFrameContainer.css("top", privacyCSSTop);
+   iFrameContainer.show();
 }
 
 function render(data) {
+   console.log("egj discord render.");
    var htmlIframeString = `
       <div id="${iFrameContainerId}" class="${iframeContainerClass} egj-discord-fade-in">
          ${data}
@@ -47,37 +61,64 @@ function render(data) {
 
    createDiv(htmlIframeString);
 
-   container = document.getElementById(iFrameContainerId);
+   const bubbleContainer = $( `#${bubbleContainerId}` ).first();
+   const iFrameContainer = $( `#${iFrameContainerId}` ).first();
 
-   setDisplay(container, "block");
+   const privacyCSSTop = bubbleContainer.position().top - iFrameContainer.height() - 10;
+   iFrameContainer.css("top", privacyCSSTop);
+   iFrameContainer.show();
 }
 
-
-discord.toggle = function(){
+discord.toggleDiscord = function() {
+   console.log("egj discord toggleDiscord")
    if(getVW() < 768 || getVH() < 900) {
-
       window.location.href = "/discord/";
       return;
-
    }
 
-   var container = document.getElementById(iFrameContainerId);
+   const iFrameContainer = $( `#${iFrameContainerId}` ).first();
 
-   if(!container) {
+   if(!iFrameContainer.length){
       getData();
    }
-   else {
-      if(container.style.display === "none" || container.style.display === '') {
-         setDisplay(container, "block");
+   else  {
+      if(iFrameContainer.is(":visible")) {
+         iFrameContainer.hide();
       }
       else {
-         setDisplay(container, "none");
+         iFrameContainer.show();
       }
    }
+
 }
 
-function setDisplay(ele, style) {
-   ele.style.display = style
+discord.showPrivacy = function() {
+   const htmlPrivacyString = `
+      <div id="${privacyContainerId}" class="egj-discord-privacy-container">
+         <span style="background: white">Akzeptieren Sie unsere <a href="datenschutz/">Datenschutzerklärung</a>? </span>
+         <div class="wp-block-button is-style-shadow w-100">
+            <a id=${privacyButtonId} class="wp-block-button__link" >Akzeptieren</a>
+         </div>
+      </div>`;
+   
+   if(!discord.privacyContainerCreated) {
+      createDiv(htmlPrivacyString);
+      discord.privacyContainerCreated = true;
+   }
+ 
+   const privacyButton = $( `#${privacyButtonId}` ).first();
+
+   privacyButton.click(() => {
+      discord.privacyAccepted = true;
+      privacyContainer.hide()
+      discord.toggleDiscord();      
+   });
+   
+   const bubbleContainer = $( `#${bubbleContainerId}` ).first();
+   const privacyContainer = $( `#${privacyContainerId}` ).first();
+
+   const privacyCSSTop = bubbleContainer.position().top - privacyContainer.height() - 10;
+   privacyContainer.css("top", privacyCSSTop);
 }
 
 function createDiv(html) {
@@ -89,7 +130,7 @@ function createDiv(html) {
 discord.init = function() {
    
    var htmlBubbleString = `
-      <div id="egjDiscordBubbleContainer" class="egj-discord-container">
+      <div id="egjDiscordBubbleContainer" class="egj-discord-bubble-container">
          <div class="egj-discord-talk-bubble egj-discord-tri egj-discord-border">
             &nbsp;            
          </div>   
@@ -99,7 +140,12 @@ discord.init = function() {
 
    const container = document.getElementById("egjDiscordBubbleContainer");
    container.addEventListener('click', event => {
-      discord.toggle();
+      if(!discord.privacyAccepted) {
+         discord.showPrivacy();
+      }
+      else {
+         discord.toggleDiscord();
+      }
    });
 
 }
